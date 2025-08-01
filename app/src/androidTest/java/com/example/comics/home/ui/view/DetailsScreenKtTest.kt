1d08b646ed646a5ffda4fa7659dc42e471d5d434
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2025-08-01 11:47:01 -0300

    feat: add onNodeWithText

--- app/src/androidTest/java/com/example/comics/home/ui/view/DetailsScreenKtTest.kt
+++ app/src/androidTest/java/com/example/comics/home/ui/view/DetailsScreenKtTest.kt
@@ -3,6 +3,7 @@ package com.example.comics.home.ui.view
 import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.example.comics.mocks.mockMovie
 import org.junit.Rule
@@ -33,5 +34,11 @@ class DetailsScreenKtTest {
         composeTestRule.onNodeWithTag(testTag = "overview", useUnmergedTree = true)
             .assertExists()
             .assertTextContains(value = mockMovie.overview.orEmpty())
+
+        composeTestRule.onNodeWithText(text = mockMovie.title.toString())
+            .assertExists()
+
+        composeTestRule.onNodeWithText(text = mockMovie.overview.toString())
+            .assertExists()
     }
 }
